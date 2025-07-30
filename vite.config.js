e005c7d4a0d3677003de4a98a275ba33c9e4df29
math-eaton <mjh2241@columbia.edu>
2025-07-29 23:49:07 -0400

    rm vite config html mapping

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        cv: resolve(__dirname, 'cv.html'),
-        about: resolve(__dirname, 'about.html')
-      }
-    }
-  }
-});
+export default defineConfig({});
